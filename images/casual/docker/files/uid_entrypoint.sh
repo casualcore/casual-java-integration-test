ca6ecee0946f8a937f4d039e3a7cba0427b43ddf
Mathias Creutz <mathiascreutz@yahoo.se>
2026-04-09 15:10:17 +0200

    ...

--- images/casual/docker/files/uid_entrypoint.sh
+++ images/casual/docker/files/uid_entrypoint.sh
@@ -16,4 +16,4 @@
 # envsubst '${CASUAL_HOME}' < /${CASUAL_DOMAIN_HOME}/configuration/nginx.conf.template > $CASUAL_HOME/nginx/conf/nginx.conf
 
 source /opt/casual/etc/bash_completion.d/casual
-exec env CASUAL_LOG_PATH=/tmp/casual.log casual-domain-manager -c "$CASUAL_DOMAIN_HOME/configuration/domain.yaml"
+exec casual-domain-manager -c "$CASUAL_DOMAIN_HOME/configuration/domain.yaml"
