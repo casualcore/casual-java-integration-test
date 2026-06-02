5a307b91cfe1ff2fb7700bb46ddb9eb568c4b51a
cklellie <christopherjkelly@gmail.com>
2026-06-02 10:19:48 +0200

    Additional test for multiple service calls around error service call. Bump casual to 1.8.15. Re enable test which works again with 1.8.15. (#4)

--- casual/casual-java-integration-test/src/integration/java/se/laz/casual/test/CasualResources.java
+++ casual/casual-java-integration-test/src/integration/java/se/laz/casual/test/CasualResources.java
@@ -19,7 +19,7 @@ public class CasualResources
     public static final String CASUAL_CONTAINER_NAME = "casual";
     public static final String CASUAL_CONTAINER_IMAGE_16 = "192.168.68.106:32000/casual:1.6.18";
     public static final String CASUAL_CONTAINER_IMAGE_17 = "192.168.68.106:32000/casual:1.7.10";
-    public static final String CASUAL_CONTAINER_IMAGE_18 = "192.168.68.106:32000/casual:1.8.13";
+    public static final String CASUAL_CONTAINER_IMAGE_18 = "192.168.68.106:32000/casual:1.8.15";
     public static final String CASUAL_CONTAINER_IMAGE = CASUAL_CONTAINER_IMAGE_18;
 
     public static final int CASUAL_CONTAINER_PORT = 7771;
